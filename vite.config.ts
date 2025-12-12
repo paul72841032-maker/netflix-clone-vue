75ae144504485adf1c9ff68ad0bfdf95a2115447
paul72841032-maker <paul72841032@gmail.com>
2025-12-12 21:03:55 +0900

    udpat4

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // Vite 설정 (Pages 배포 시 base만 바꾸면 됨)
 export default defineConfig({
     plugins: [vue()],
-    base: process.env.VITE_APP_BASE || '/',
+    base: process.env.VITE_APP_BASE || '/netflix-clone-vue/', // ★ GitHub Pages 경로
 })
